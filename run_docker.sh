c20579680d9a02275f53627b38b6496854044024
Eric Schleicher <eric@constructiverealities.io>
2022-06-06 18:09:39 -0700

    make script match github tag

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 xhost +local:root
-docker run  -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.Xauthority:/root/.Xauthority -it --rm --privileged -e DISPLAY=$DISPLAY ghcr.io/constructiverealities/depthai_tof_demo:latest $@
+docker run  -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.Xauthority:/root/.Xauthority -it --rm --privileged -e DISPLAY=$DISPLAY ghcr.io/constructiverealities/depthai_tof_demo:main $@
 xhost -local:root
